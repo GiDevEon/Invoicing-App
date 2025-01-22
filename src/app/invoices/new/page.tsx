ebe4eab8d8f00d7dde81a677dbf9037edda4a944
GiDevEon <gideonnfarns@gmail.com>
2025-01-22 23:08:52 +0100

    test

--- src/app/invoices/new/page.tsx
+++ src/app/invoices/new/page.tsx
@@ -1,3 +1,6 @@
+import { sql } from "drizzle-orm";
+import { db } from "../../../db";
+
 import { Label } from "@/components/ui/label"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
@@ -5,7 +8,8 @@ import { Textarea } from "@/components/ui/textarea"
 
 
 
-export default function Home() {
+export default async function Home() {
+  const results = await db.execute(sql`SELECT current_database()`)
   return (
     <main className="flex flex-col justify-center h-full gap-6 max-w-5xl mx-auto my-12">
       <div className="flex justify-between w-full">
@@ -14,27 +18,29 @@ export default function Home() {
             </h1>
         </div>
 
+        { JSON.stringify(results)}
+
         <form className="grid gap-4 max-w-xs">
             <div>
-             <Label htmlFor="name" id="name" name="name" className="block font-semibold text-sm mb-2">
+             <Label htmlFor="name" id="name" className="block font-semibold text-sm mb-2">
                 Billing Name
              </Label>
              <Input type="text" />   
             </div>
             <div>
-             <Label htmlFor="email" id="email" name="email" className="block font-semibold text-sm mb-2">
+             <Label htmlFor="email" id="email "className="block font-semibold text-sm mb-2">
                 Billing Email
              </Label>
              <Input type="email" />   
             </div>
             <div>
-             <Label htmlFor="value" id="value" name="value" className="block font-semibold text-sm mb-2">
+             <Label htmlFor="value" id="value" className="block font-semibold text-sm mb-2">
                 Value
              </Label>
              <Input type="text" />   
             </div>
             <div>
-             <Label htmlFor="description" id="description" name="value" className="block font-semibold text-sm mb-2">
+             <Label htmlFor="description" id="description" className="block font-semibold text-sm mb-2">
                 Description
              </Label>
              <Textarea ></Textarea> 
